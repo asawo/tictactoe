b9fe47f2dc37cbfcb7f339756cfbd7a0d880024a
asawo <asawo@mercari.com>
2019-10-22 13:41:02 +0900

    added main game logic + disabled the rest of the buttons after a player wins

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 /*
 Game function
-- Player clicks buttons to mark with X or O
+- Player clicks buttons to mark with X or O ✅
 - Player to get 3 X or O in a row wins
 - Notify player when they win
 - Add score when a player wins
-- Clear scores
+- Clear the grid and scores with a reset button  ✅
 */
 
 //UI elements
@@ -16,7 +16,6 @@ for (const button of gridBtn) {
   button.addEventListener("click", move);
 }
 
-// Alternate between button text
 let buttonText = "O",
   turn = 0,
   game = 0;
@@ -30,11 +29,96 @@ function move(e) {
   }
   document.getElementById(e.target.id).disabled = true;
   turn += 1;
+  getWinner();
   console.log(e.target.id + " pushed, turn " + turn);
 }
 
-//Add event listener for node list returned by querySelectorll
-// grid.addEventListener("click", move);
+//find winner
+function getWinner() {
+  const btn1 = document.querySelector("#btn1"),
+    btn2 = document.querySelector("#btn2"),
+    btn3 = document.querySelector("#btn3"),
+    btn4 = document.querySelector("#btn4"),
+    btn5 = document.querySelector("#btn5"),
+    btn6 = document.querySelector("#btn6"),
+    btn7 = document.querySelector("#btn7"),
+    btn8 = document.querySelector("#btn8"),
+    btn9 = document.querySelector("#btn9");
+
+  if (
+    btn1.innerHTML !== "" &&
+    btn1.innerHTML == btn4.innerHTML &&
+    btn1.innerHTML == btn7.innerHTML
+  ) {
+    console.log(btn1.innerHTML + " win");
+    for (const button of gridBtn) {
+      button.disabled = true;
+    }
+  } else if (
+    btn2.innerHTML !== "" &&
+    btn2.innerHTML == btn5.innerHTML &&
+    btn2.innerHTML == btn8.innerHTML
+  ) {
+    console.log(btn2.innerHTML + " win");
+    for (const button of gridBtn) {
+      button.disabled = true;
+    }
+  } else if (
+    btn3.innerHTML !== "" &&
+    btn3.innerHTML == btn6.innerHTML &&
+    btn3.innerHTML == btn9.innerHTML
+  ) {
+    console.log(btn3.innerHTML + " win");
+    for (const button of gridBtn) {
+      button.disabled = true;
+    }
+  } else if (
+    btn1.innerHTML !== "" &&
+    btn1.innerHTML == btn2.innerHTML &&
+    btn1.innerHTML == btn3.innerHTML
+  ) {
+    console.log(btn1.innerHTML + " win");
+    for (const button of gridBtn) {
+      button.disabled = true;
+    }
+  } else if (
+    btn4.innerHTML !== "" &&
+    btn4.innerHTML == btn5.innerHTML &&
+    btn4.innerHTML == btn6.innerHTML
+  ) {
+    console.log(btn4.innerHTML + " win");
+    for (const button of gridBtn) {
+      button.disabled = true;
+    }
+  } else if (
+    btn7.innerHTML !== "" &&
+    btn7.innerHTML == btn8.innerHTML &&
+    btn7.innerHTML == btn9.innerHTML
+  ) {
+    console.log(btn7.innerHTML + " win");
+    for (const button of gridBtn) {
+      button.disabled = true;
+    }
+  } else if (
+    btn1.innerHTML !== "" &&
+    btn1.innerHTML == btn5.innerHTML &&
+    btn1.innerHTML == btn9.innerHTML
+  ) {
+    console.log(btn1.innerHTML + " win");
+    for (const button of gridBtn) {
+      button.disabled = true;
+    }
+  } else if (
+    btn3.innerHTML !== "" &&
+    btn3.innerHTML == btn5.innerHTML &&
+    btn3.innerHTML == btn7.innerHTML
+  ) {
+    console.log(btn3.innerHTML + " win");
+    for (const button of gridBtn) {
+      button.disabled = true;
+    }
+  }
+}
 
 //Reset
 resetBtn.addEventListener("click", function() {
